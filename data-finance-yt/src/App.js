17fc8214580db6ee4d1575fef43b46c796b325a2
EmberDes <shashwatsinghdit@gmail.com>
2025-01-17 16:56:20 +0000

    1st commit

---
+++ data-finance-yt/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-white'>React</h1>
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ data-finance-yt/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React  from "react";
+
+const navbar = () =>{
+    return(
+        <div className="text-white">
+            <h1>React.</h1>
+        </div>
+    )
+}
+
+export default navbar

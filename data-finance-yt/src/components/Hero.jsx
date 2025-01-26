a008b73cf36b89f27d81800493232591c1f37b87
EmberDes <shashwatsinghdit@gmail.com>
2025-01-26 17:40:55 +0000

    FinalDay-React + tailwind

--- data-finance-yt/src/components/Hero.jsx
+++ data-finance-yt/src/components/Hero.jsx
@@ -19,9 +19,11 @@ export const Hero = () => {
           className='md:text-5xl sm:text-4xl text-xl font-bold py-4 pl-2'
           strings= {[' BTB' , ' BTC' , ' SAAS']} typeSpeed={120} backSpeed={140} loop />
         </div>
-        <p>
-          Monitor your Data-Analytics to Increase revenue for BTB, BTC & SAAS Platoforms.
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>
+          Monitor your Data-Analytics to Increase revenue for BTB, BTC & SAAS Platforms.
         </p>
+
+        <button className='bg-[#00df9a]  w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
     </div>
   </div>
   )
